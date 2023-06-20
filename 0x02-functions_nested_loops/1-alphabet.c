8081ac6f0a863451970f8b42d258588bb049959f
Samstiles-Dev <Ayosamuel777@gmail.com>
2023-06-20 04:08:50 -0700

    1-alpahbet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//#include <main.h>
+
+int main(void) {
+    print_alphabet();
+    return 0;
+}
+
+void print_alphabet(void) {
+    char alp[]="abcdefghijklmnopqrstuvwxyz";
+    printf("%s\n",alp);
+}
