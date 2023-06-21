f3290a80e7c2dec9e99cd943d9e775358ea6d2fd
Samstiles-Dev <Ayosamuel777@gmail.com>
2023-06-21 11:06:57 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
-{
-    print_alphabet_x10();
-    return (0);
-}
 
-void print_alphabet_x10(void){
-    char alp[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i = 0;
-    while(i < 10 ) {
-        printf(alp);
-        i++;
-    }
-}
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+	void print_alphabet_x10(void)
+	{
+		char ch;
+		int i;
+
+
+		i = 0;
+
+
+		while (i < 10)
+		{
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
