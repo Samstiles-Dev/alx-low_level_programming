68551513ffae0bf024c3c1596bf724529b2dd7c8
Samstiles-Dev <Ayosamuel777@gmail.com>
2023-08-02 16:19:31 -0700

    delete nodelist

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,44 +1,48 @@
-10-delete_nodeint.c
-
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - a function that deletes a node in a linked list using the index
+ * delete_nodeint_at_index - a function that deletes a node in a linked list using an index
  * @head: a pointer to the first element in the linked list
- * @index: an index of the node to be deleted
+ * @index: index of the node to delete
  *
  * Return: 1, or -1
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *rem_314 = *head;
-	listint_t *current_314 = NULL;
-	unsigned int _i_314 = 0;
+	listint_t *temp_244 = *head;
+	listint_t *current = NULL;
+	unsigned int i_244 = 0;
 
 	if (*head == NULL)
-	{
+	{	
+		/* Empty list, cannot delete */
 		return (-1);
 	}
 
 	if (index == 0)
-	{
+	{	
+		/* Deleting the head node */
 		*head = (*head)->next;
-		free(rem_314);
+		free(temp_244);
 		return (1);
 	}
 
-	while (_i_314 < index - 1)
+	/* Traverse to find the previous node */
+	while (i_244 < index - 1)
 	{
-		if (!rem_314 || !(rem_314->next))
+		if (!temp_244 || !(temp_244->next))
+		{
+			/* Index out of bounds, cannot delete */
 			return (-1);
-		rem_314 = rem_314->next;
-		_i_314++;
+		}
+		temp_244 = temp_244->next;
+		i_244++;
 	}
 
-
-	current_314 = rem_314->next;
-	rem_314->next = current_314->next;
-	free(current_314);
+	/* Delete the node */
+	current = temp_244->next;
+	temp_244->next = current->next;
+	free(current);
 
 	return (1);
 }
