1d236a3c6bad7897567347fa2945e1b05bee407a
Samstiles-Dev <ayosamuel777@gmail.com>
2023-08-31 10:49:22 -0700

    0-binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - this function converts a binary no to unsigned integer
+ * @b: a string argument that contains the binary no
+ *
+ * Return: the function returns the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	/* variable declaration */ 
+	int num_221;
+	unsigned int decimal_number_32 = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+
+	for (num_221 = 0; b[num_221]; num_221++)
+	{
+		if (b[num_221] < '0' || b[num_221] > '1')
+		{
+			return (0);
+		}
+		decimal_numeber_32 = 2 * decimal_numeber_32 + (b[num_221] - '0');
+	}
+
+	return (decimal_numeber_32);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,10 @@
+Project -> 14. C - Bit manipulation
+Tasks -> 7
+0 -> 0-binary_to_uint.c
+1 -> 1-print_binary.c
+2 -> 2-get_bit.c
+3 -> 3-set_bit.c
+4 -> 4-clear_bit.c
+5 -> 5-flip_bits.c
+6 -> 100-get_endianness.c
+7 -> 101-password
