7db09399f6bece7ecd94a811f27154b6c5efd79b
Samstiles-Dev <ayosamuel777@gmail.com>
2023-10-20 16:44:52 -0700

    updated

--- 0x18-dynamic_libraries/reallocates.c
+++ 0x18-dynamic_libraries/reallocates.c
@@ -2,18 +2,18 @@
 
 /**
  * *_realloc - allocates a memory block using malloc and free.
- * @pointer: pointer to memory allocated with malloc(former_size).
- * @former_size: size in bytes, of the allocated space for pointer
+ * @pointer: pointer to memory allocated with malloc(fmr_size).
+ * @fmr_size: size in bytes, of the allocated space for pointer
  * @latest_size: size in bytes of the new memory block.
  *
  * Return: NULL if latest_size = 0 and pointer is not NULL.
  */
-void *_realloc(void *pointer, unsigned int former_size, unsigned int latest_size)
+void *_realloc(void *pointer, unsigned int fmr_size, unsigned int latest_size)
 {
 	void *p;
 	unsigned int i;
 
-	if (latest_size == former_size)
+	if (latest_size == fmr_size)
 		return (pointer);
 	if (latest_size == 0 && pointer != NULL)
 	{
@@ -27,12 +27,12 @@ void *_realloc(void *pointer, unsigned int former_size, unsigned int latest_size
 			return (NULL);
 		return (p);
 	}
-	if (latest_size > former_size)
+	if (latest_size > fmr_size)
 	{
 		p = malloc(latest_size);
 		if (p == NULL)
 			return (NULL);
-		for (i = 0; i < former_size && i < latest_size; i++)
+		for (i = 0; i < fmr_size && i < latest_size; i++)
 			*((char *)p + i) = *((char *)pointer + i);
 		free(pointer);
 	}
