23366dc6bcca19b5ccf358fae68ab5a4d4e3a771
Samstiles-Dev <ayosamuel777@gmail.com>
2024-02-10 03:16:03 -0800

    search algo

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -2,45 +2,45 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - Searches a value in a sorted linked-list with an \
+ * linear_skip - Func searches a value in a sorted linked-list with an \
  * express lane using a linear search.
  * @list: The linked list with an express lane to search in.
- * @value: The value to search for.
+ * @value: The value to look for.
  *
  * Return: The node with the value in the linked list, otherwise NULL.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	size_t i, step, a = 0, b = 0;
-	skiplist_t *_node, *_next;
+	size_t i, step, c = 0, d = 0;
+	skiplist_t *node, *next;
 
 	if (!list)
 		return (NULL);
-	_node = list;
-	_next = _node->express ? _node->express : _node;
-	while (_next)
+	node = list;
+	next = node->express ? node->express : node;
+	while (next)
 	{
-		printf("Value checked at index [%d] = [%d]\n", (int)_next->index, _next->n);
-		if (_next->n >= value)
+		printf("Value checked at index [%d] = [%d]\n", (int)next->index, next->n);
+		if (next->n >= value)
 			break;
-		_node = _next;
-		if ((_node->n < value) && (_node->express == NULL))
+		node = next;
+		if ((node->n < value) && (node->express == NULL))
 		{
-			while (_next->_next)
-				_next = _next->_next;
+			while (next->next)
+				next = next->next;
 			break;
 		}
-		_next = _node->express ? _node->express : _node;
+		next = node->express ? node->express : node;
 	}
-	a = _node->index;
-	b = _next->index;
-	printf("Value found between indexes [%d] and [%d]\n", (int)a, (int)b);
-	while (_node)
+	c = node->index;
+	d = next->index;
+	printf("Value found between indexes [%d] and [%d]\n", (int)c, (int)d);
+	while (node)
 	{
-		printf("Value checked at index [%d] = [%d]\n", (int)_node->index, _node->n);
-		if (_node->n == value)
-			return (_node);
-		_node = _node->_next;
+		printf("Value checked at index [%d] = [%d]\n", (int)node->index, node->n);
+		if (node->n == value)
+			return (node);
+		node = node->next;
 	}
 	return (NULL);
 }
